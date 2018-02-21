48887ba41de146a9b8fc97fac63deab636bd97bb
slom <slom72@gmail.com>
2018-02-21 21:45:22 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :rental_units
+  namespace 'api' do
+    namespace 'v1' do
+      resources :trips
+    end
+  end
 end
+
+
