c14bc7603463b1edfd46a58e28f1ae9623c7f91f
slom <slom72@gmail.com>
2018-02-22 05:22:48 +0200

    added needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :rental_units
+  require 'sidekiq/web'
+  mount Sidekiq::Web => "/sidekiq"
   namespace 'api' do
     namespace 'v1' do
       resources :trips
+      get "/report" => "trips#report", as: "trips_report"
     end
   end
 end
